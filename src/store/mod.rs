2276fc8ebba23bc019dbf151d2990f753afb5d3f
lucabecci <beccibrian@gmail.com>
2025-04-01 01:02:55 -0300

    Update Store trait to return Option for get method, enhancing error handling

--- src/store/mod.rs
+++ src/store/mod.rs
@@ -1,9 +1,11 @@
+use crate::store::types::{CacheEntry, CacheKey};
+
 pub mod lfu;
 pub mod lru;
 pub mod types;
 
 pub trait Store {
-    fn get(&mut self, key: &CacheKey) -> Result<$CacheEntry>;
+    fn get(&mut self, key: &CacheKey) -> Option<&CacheEntry>;
     fn put(&mut self, key: CacheKey, value: CacheEntry);
     fn remove(&mut self, key: &CacheKey);
     fn len(&self) -> usize;
